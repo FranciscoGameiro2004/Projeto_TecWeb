e928fc5331f3381fa36e31fbe044fc95869ba5b0
BensonTerra <benson.terra@hotmail.com>
2022-11-02 11:00:57 +0000

    0

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,15 +1,15 @@
 window.onscroll = function() {myFunction()};
-                
-                var topnav = document.getElementById("topnav");
-                var sticky = topnav.offsetTop;
-                
-                function myFunction() {
-                  if (window.pageYOffset >= sticky) 
-                  {
-                    topnav.classList.add("sticky")
-                  }
-                  else 
-                  {
-                    topnav.classList.remove("sticky");
-                  }
-                }
+
+var topnav = document.getElementById("topnav");
+var sticky = topnav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) 
+  {
+    topnav.classList.add("sticky")
+  }
+  else 
+  {
+    topnav.classList.remove("sticky");
+  }
+}
