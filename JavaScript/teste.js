f3334e5a54008bc710d1ee80e408d34cafeda32c
BensonTerra <benson.terra@hotmail.com>
2023-01-14 18:53:28 +0000

    aaa

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -1,12 +1,26 @@
-let textoPesquisa;
+const filtrar = document.getElementById("btnSearch")
+const btn1 = document.getElementById("btn1")
+const btn2 = document.getElementById("btn2")
+const btn3 = document.getElementById("btn3")
 
-document.getElementById("btnSearch").onclick = function() {
-    textoPesquisa = document.getElementById("textoSearch").value;
-    if (textoPesquisa == "") {
-        document.getElementById("change").innerHTML = "centerSide";
+
+function teste() {
+    const textChange = document.getElementById("textChange");
+    const textoSearch = document.getElementById("textoSearch").value;
+
+    if (textoSearch == "") {
+        textChange.innerHTML = "leftSide"
     }
     else {
-        console.log(textoPesquisa);
-        document.getElementById("change").innerHTML = textoPesquisa;
+        textChange.innerHTML = textoSearch
     }
+
 }
+function testeClick(texto) {
+    console.log(texto)
+}
+
+filtrar.addEventListener('click',teste)
+btn1.addEventListener('click',function() {testeClick('a')})
+btn2.addEventListener('click',function() {testeClick('b')})
+btn3.addEventListener('click',function() {testeClick('c')})
