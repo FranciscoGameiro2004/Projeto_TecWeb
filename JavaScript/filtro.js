9e432b394205b5e3dba721504881021edac77674
BensonTerra <benson.terra@hotmail.com>
2023-01-14 14:07:00 +0000

    hg

--- JavaScript/filtro.js
+++
@@ -1,8 +0,0 @@
-var produtoSearch;
-
-document.getElementById("btnSearch").onclick = function() 
-    {
-        textoPesquisa = document.getElementById("textoSearch").value;
-        textoProduto = document.getElementById("produtoNome").value;
-        console.log(textoPesquisa);
-    }
