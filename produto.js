377ec03d808ba92f8589c75b6127155bfb2d3623
FranciscoGameiro2004 <40220426@esmad.ipp.pt>
2023-01-14 14:09:12 +0000

    Criação da página do produto.

---
+++ produto.js
@@ -0,0 +1,30 @@
+function associarProduto(codigo){
+    /*
+    Organização de cada produto:
+    [Código do produto],[Nome do produto],[Preço],[[Existe Promocao?],[Preço original],[% Desconto]],[[Thumbnail],[[Foto 1],[Foto 2], ... , [Foto N]],[Avaliação],[Detalhes]
+    */
+    listaProdutos = [['01','Porta chaves 1CM', '3,00€', ['False'], ['https://www.theuncomfortable.com/wp-content/uploads/2018/10/Ruler-1024x1024.jpg'],3],
+                     ['02','Conjunto Vasos Caneca Longa', '15,00€', ['True', '20,00€', '25%'], ['https://www.theuncomfortable.com/wp-content/uploads/2017/05/teaset_01_p-1024x687.jpg','https://www.theuncomfortable.com/wp-content/uploads/2017/04/24_long_mug.jpg'],4.5]]
+    
+    for (let i of listaProdutos){
+        console.log(i)
+        if (i[0] == codigo){
+            console.log(`Código do produto: ${i[0]}`)
+            document.getElementById('nomeProdutoDestacado').innerHTML = i[1]
+            document.getElementById('avaliacaoProdutoDestacado').innerHTML = `Avaliação: ${i[5]}`
+            document.getElementById('imagemProdutoDestacado').src = i[4][0]
+            document.getElementById('precoProdutoDestacado').innerHTML = i[2]
+            if (i[3][0] == 'True'){
+                document.getElementById('precoOriginalProdutoDestacado').innerHTML = i[3][1]
+                document.getElementById('descontoProdutoDestacado').innerHTML = `${i[3][2]} de DESCONTO!!!`
+                document.getElementById('precoOriginalProdutoDestacado').style.visibility = 'visible'
+                document.getElementById('descontoProdutoDestacado').style.visibility = 'visible'
+            }
+            break
+        }
+    }
+}
+
+const urlPar = new URLSearchParams(window.location.search)
+codigo = urlPar.get('produto')
+associarProduto(codigo)
