c1876d248fdcc427edf2577928dece5cdd1b4d34
BensonTerra <benson.terra@hotmail.com>
2023-01-18 22:40:39 +0000

    carrinho in progress

--- JavaScript/carrinho.js
+++ JavaScript/carrinho.js
@@ -1,4 +1,4 @@
-iniciar()
+start()
 
 function start() {
     if (document.readyState == 'loading') {
@@ -8,25 +8,21 @@ function start() {
     }
 }
 
+function testeClick() {
+    console.log("testeClick ");
+}
+
 function ready() {
-    
-    var addToCartButtons = document.getElementsByClassName('shop-item-button')
-    for (var i = 0; i < addToCartButtons.length; i++) {
-        var button = addToCartButtons[i]
-        button.addEventListener('click', addToCartClicked)
+    var adicionarProduto = document.getElementsByClassName("btnAdd")
+    for (var i = 0; i < adicionarProduto.length; i++) 
+    {
+        var button = adicionarProduto[i]
+        button.addEventListener("click", function () { testeClick() })
     }
 
-    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-    for (var i = 0; i < removeCartItemButtons.length; i++) {
-        var button = removeCartItemButtons[i]
-        button.addEventListener('click', removeCartItem)
-    }
+}
 
-    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-    for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInputs[i]
-        input.addEventListener('change', quantityChanged)
-    }
-
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+function adicionarProdutoClicado(event) {
+    var botao = event.target
+    var 
 }
